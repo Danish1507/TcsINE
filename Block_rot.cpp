4706268efa0e0e055e5def9656ad7b8ae81f59d1
danish dk <mohmdanish0123@gmail.com>
2025-03-08 03:08:41 +0530

    block rotation

---
+++ Block_rot.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+int n;
+cin>>n;
+vector<int> v(n);
+for(int i=0; i<n; i++){
+  cin>>v[i];
+ }
+
+ int k;
+ cin>>k;
+
+reverse(v.begin(),v.begin()+k);
+reverse(v.begin()+k,v.end());
+reverse(v.begin(),v.end());
+
+for(int i=0; i<n; i++){
+  cout<<v[i]<<" ";
+ }
+
+return 0;
+}
